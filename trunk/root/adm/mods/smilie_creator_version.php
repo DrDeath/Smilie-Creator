3300f494c966d31a7b61da96a214b0e93debaa9b
Dr.Death <Dr.Death@lpi-clan.de>
2010-12-21 13:35:31 +0100

    Revision 1_0_8 - Not validated from phpBB.com MOD Team.

--- trunk/root/adm/mods/smilie_creator_version.php
+++ trunk/root/adm/mods/smilie_creator_version.php
@@ -2,11 +2,16 @@
 /**
 *
 * @package acp
-* @version $Id$
+* @version $Id: smilie_creator_version.php 63 2008-08-02 16:11:31Z stoffel04 $
 * @copyright (c) 2005 phpBB Group
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
 */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
 
 /**
 * @package smilie_creator
@@ -19,7 +24,7 @@ class smilie_creator_version
 			'author'	=> 'Dr.Death',
 			'title'		=> 'Smilie Creator',
 			'tag'		=> 'smilie_creator',
-			'version'	=> '1.0.6',
+			'version'	=> '1.0.8',
 			'file'		=> array('www.lpi-clan.de', 'updatecheck', 'smilie_creator.xml'),
 		);
 	}
